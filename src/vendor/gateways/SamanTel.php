d21c72cea72261c1f61d0b45a392443f1bdff2aa
Amirhossein Jahani <me@iamir.net>
2024-01-01 10:49:52 +0330

    up

--- src/vendor/gateways/SamanTel.php
+++ src/vendor/gateways/SamanTel.php
@@ -20,7 +20,7 @@ class SamanTel extends \iAmirNet\SMS\Gateways\SabaPayamak
     {
         parse_str($model->token, $identy);
         if ((!isset($identy['username']) && (isset($identy['username']) && !strlen($identy['username']))) || !isset($identy['password']) && (isset($identy['password']) && !strlen($identy['password']))) {
-            throw new iException("Please enter valid Usernaem & Password in sms method({$model->serial}).");
+            throw new iException("Please enter valid Username & Password in sms method({$model->serial}).");
         }
         parent::__construct(array_merge([
             'number' => $model->number,
